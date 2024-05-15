b3f34636fddd2b9074435f24ed29c6e1e0ccb010
KaungKyawHtin <softguide.kaungkyawhtin@gmail.com>
2024-05-15 13:26:05 +0630

    first commit of airbnb-clone setting with tailwindcss and adding navbar

---
+++ config/tailwind.config.js
@@ -0,0 +1,30 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        ...colors,
+        primary: {
+          DEFAULT: '#FF5A5F',
+          dark: '#FF385C',
+        }
+      },
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+  ]
+}
